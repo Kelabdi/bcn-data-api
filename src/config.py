1407a5834a9613f3960e9e2b5d34d369ce335ee6
Kelabdi <ingeniero.karim@gmail.com>
2021-08-10 22:06:22 +0200

    Mongo Atlas upload and population dataset review

--- src/config.py
+++ src/config.py
@@ -4,3 +4,8 @@ import dotenv
 dotenv.load_dotenv()
 
 PORT = os.getenv("PORT")
+USER = os.getenv("USER")
+PASS = os.getenv("PASS")
+
+mongo_url = f"mongodb+srv://{USER}:{PASS}@bdmlpt0521midproject.tgi9t.mongodb.net/"
+
---
+++ src/mongo_connection.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+from config import mongo_url
+
+client = MongoClient(mongo_url)
+
+
+
